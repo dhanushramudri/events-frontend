533828e643a9b4e7d994a9c07f60a0c8f12d301b
dhanushramudri <ramudridhanush@gmail.com>
2025-04-14 14:05:49 +0530

    updated users fetch

--- src/admin/services/userService.js
+++ src/admin/services/userService.js
@@ -2,9 +2,12 @@ import axios from "axios";
 import { API_URL } from "../config/constants";
 
 export const getUsers = async () => {
-  const API_URL = `${API_URL}`;
   try {
     const res = await axios.get(`${API_URL}/admin/my-events/participants`);
+    console.log("res is ", res);
+    if (!res || !res.data) {
+      throw new Error("Invalid response from server");
+    }
     return res.data.participants || [];
   } catch (error) {
     console.error("Error fetching users:", error);
