bb550b2aa33707aceabb76402f86e47debc5d6ce
ramudridhanush <ramudridhanush@gmail.com>
2025-04-15 19:08:39 +0530

    resolved deployment

--- src/admin/pages/LandingPage.jsx
+++ src/admin/pages/LandingPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Button } from "../components/ui/Button";
+import { Button } from "../components/ui/button";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 
