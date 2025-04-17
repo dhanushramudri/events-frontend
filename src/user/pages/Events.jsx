7f0bb5d4819a6b2102332636a5bd60cd88348339
ramudridhanush <ramudridhanush@gmail.com>
2025-04-17 12:56:37 +0530

    updated user module

--- src/user/pages/Events.jsx
+++ src/user/pages/Events.jsx
@@ -129,7 +129,7 @@ const Events = () => {
       )}
 
       {/* Events Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 " >
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 " >
         {currentEvents.map((event) => (
           <EventCard key={event.id} event={event} isFavorite={favorites.includes(event.id)} />
         ))}
